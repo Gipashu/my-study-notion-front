5d092bb66c97644380f9a04ad8e2bdf64145180d
Gipashu Aryan <gipashuaryan@gmail.com>
2024-04-11 14:29:19 +0530

    second

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,6 @@
 // require("dotenv").config();
-const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "https://my-study-notion-bck.onrender.com/api/v1"
+// const BASE_URL = "http://localhost:4000/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
